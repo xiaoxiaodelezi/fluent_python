7b9296d5750d855fd10dfb9a7855f45ae3c74019
xiaoxiaodelezi <eachdayachance@hotmail.com>
2023-01-04 13:58:53 +0800

    20230104

---
+++ CH9/9_13.py
@@ -0,0 +1,77 @@
+# 设定从类中继承的typecode属性，自定义一个实例属性
+
+from array import array
+import math
+
+
+class Vector2d:
+    typecode = 'd'
+
+    def __init__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r},{!r})'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+                bytes(array(self.typecode, self)))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}. {}>'
+        else:
+            coords = self
+            outer_fmt = '({},{})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+
+v1 = Vector2d(1.1, 2.2)
+dump = bytes(v1)
+print(dump)
+print(len(dump))
+v1.typecode = 'f'
+dumpf = bytes(v1)
+print(dumpf)
+print(len(dumpf))
+print(Vector2d.typecode)
